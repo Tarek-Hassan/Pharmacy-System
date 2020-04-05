702cbad5cebfa1a0c4a230e9bbaae3e5184785ab
mohamed <mohamedahmed4362@gmail.com>
2020-04-05 16:54:54 +0200

    fixed update

--- app/Http/Requests/MedicineRequest.php
+++ app/Http/Requests/MedicineRequest.php
@@ -13,7 +13,7 @@ class MedicineRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -13,7 +13,7 @@ class OrderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
