2ab7674a5e3862f9e493d7f3a8b1fde17c8feb5c
amira <amira.a.a.hashim@gmail.com>
2020-04-05 11:19:49 +0200

    using sanctum for access token, use sanctum middleware for show method

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +22,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/users', 'API\UserController@index');
-Route::get('users/{user}', 'API\UserController@show');
+Route::get('users/{user}', 'API\UserController@show')->middleware('auth:sanctum');;
 Route::post('/users', 'API\UserController@store');
-// Route::get("/users/{user}/edit", "API\UserController@edit");
 Route::put('/users/{user}','API\UserController@update');
 Route::delete('users/{user}', 'API\UserController@destroy');
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
